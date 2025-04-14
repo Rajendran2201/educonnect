3124f84dea3aa6591101f26d5a944e881174c032
Raj <asrajendrayadav@gmail.com>
2025-04-15 01:08:59 +0530

    feat: modules updated

--- myapp/views.py
+++ myapp/views.py
@@ -24,3 +24,40 @@ def contact(request):
 
 def eventbooking(request):
    return render(request, "eventbooking.html")
+
+def discussion_login(request):
+   return render(request, "discussion_login.html")
+
+def discussion_forum(request):
+   return render(request, "discussion_forum.html")
+
+def donate(request):
+   return render(request, "donate.html")
+
+def resources(request):
+   return render(request, "resources.html")
+
+def events(request):
+   return render(request, "events.html")
+
+
+def event1(request):
+   return render(request, "event1.html")
+
+def event2(request):
+   return render(request, "event2.html")
+
+def event3(request):
+   return render(request, "event3.html")
+
+def event4(request):
+   return render(request, "event4.html")
+
+def event5(request):
+   return render(request, "event5.html")
+
+def event6(request):
+   return render(request, "event6.html")
+
+def careerguidance(request):
+   return render(request, "careerguidance.html")
